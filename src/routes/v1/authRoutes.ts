1a5d5d4885b42f5ebe6f03e5a5ded2007be86f82
Miss Kniz <mehak313naqvi@gmail.com>
2026-04-06 14:42:45 +0500

    Added login route and updated register route

---
+++ src/routes/v1/authRoutes.ts
@@ -0,0 +1,9 @@
+import express, { Router, Request, Response } from "express";
+
+const router: Router = Router();
+
+router.post("/register", (req: Request, res: Response) => {});
+
+router.post("/login", (req: Request, res: Response) => {});
+
+export default router;
