0d1118e7107a8ecc02ef1ccb71d2d66964e45c3f
Miss Kniz <mehak313naqvi@gmail.com>
2026-04-06 14:47:27 +0500

    Updated PrismaClient configuration for logging

---
+++ src/config/db.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client/extension";
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "info", "warn", "error"]
+      : ["error"],
+});
